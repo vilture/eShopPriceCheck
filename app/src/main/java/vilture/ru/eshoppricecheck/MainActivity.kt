9188b66df9f4d5b24fd17afbec44c3b685b89900
SpiderPig <leemon.rdtc@gmail.com>
2019-01-24 00:00:32 +0700

    2

--- app/src/main/java/vilture/ru/eshoppricecheck/MainActivity.kt
+++ app/src/main/java/vilture/ru/eshoppricecheck/MainActivity.kt
@@ -1,5 +1,6 @@
 package vilture.ru.eshoppricecheck
 
+import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.Context
 import android.graphics.Bitmap
@@ -7,6 +8,7 @@ import android.net.ConnectivityManager
 import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.webkit.WebResourceRequest
@@ -15,51 +17,55 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
 
-    val web = webView
-    var url = "https://eshop-prices.com/"
+    private var url = "https://eshop-prices.com"
 
     // выполняем переключения валют
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    private val mOnNavigationItemSelectedListener = OnNavigationItemSelectedListener { item ->
+        val i = url.indexOf("?currency")
+        val sb = StringBuilder(url)
+        if (i > 0) {
+            sb.delete(i, 100)
+            url = sb.toString()
+        }
+
         when (item.itemId) {
             R.id.navigation_local -> {
-                url = "https://eshop-prices.com/"
+                webView.loadUrl("$url?currency=LOCAL")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_rub -> {
-                url = "https://eshop-prices.com/prices?currency=RUB"
+                webView.loadUrl("$url?currency=RUB")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_usd -> {
-                url = "https://eshop-prices.com/prices?currency=USD"
+                webView.loadUrl("$url?currency=USD")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_eur -> {
-                url = "https://eshop-prices.com/prices?currency=EUR"
+                webView.loadUrl("$url?currency=EUR")
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // определяем строку прогресса
         progressBar.progress = 0
-
         val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
-        //bottomNavigation.setSelectedItemId(R.id.navigation_local)
+        bottomNavigation.selectedItemId = R.id.navigation_local
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         // определяем webview
         // загружаем страничку с ценами
-        web.webViewClient = MyWebViewClient()
-
-        web.loadUrl(url)
+        webView.webViewClient = MyWebViewClient()
+        webView.loadUrl("$url/prices?currency=LOCAL")
 
         // определяем параметр настроек webview
         // включаем javascript
@@ -89,7 +95,6 @@ class MainActivity : AppCompatActivity() {
             progressBar.visibility = View.VISIBLE
         }
 
-
         override fun onPageFinished(view: WebView, url: String) {
             super.onPageFinished(view, url)
 
@@ -113,11 +118,13 @@ class MainActivity : AppCompatActivity() {
                 "javascript:(function() { " +
                         "document.getElementsByClassName('adsbygoogle')[0].style.display='none'; })()"
             )
+
         }
 
         // обрабатываем все ссылки внутри приложения
         override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
             view.loadUrl(request.url.toString())
+            url = request.url.toString()
             return true
         }
     }
@@ -131,8 +138,39 @@ class MainActivity : AppCompatActivity() {
 
     // кнопку назад отрабатываем правильно
     override fun onBackPressed() {
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+        val mWebBackForwardList = webView.copyBackForwardList()
+        var undoUrl = ""
+
+        if (url.contains("https://eshop-prices.com/games/")) {
+            val i = webView.url.indexOf("?currency")
+            val sb = StringBuilder(webView.url)
+
+            if (i > 0) {
+                sb.delete(0, i)
+                webView.loadUrl("https://eshop-prices.com/prices" + sb.toString())
+            } else {
+                sb.clear()
+                webView.loadUrl("https://eshop-prices.com/prices?currency=LOCAL")
+            }
+            url = "https://eshop-prices.com/prices"
+            return
+        }
+
         if (webView.canGoBack()) {
             webView.goBack()
+
+            if (mWebBackForwardList.currentIndex > 0)
+                undoUrl = mWebBackForwardList.getItemAtIndex(mWebBackForwardList.currentIndex - 1).url
+
+            if (undoUrl.contains("=LOCAL"))
+                bottomNavigation.selectedItemId = R.id.navigation_local
+            if (undoUrl.contains("=RUB"))
+                bottomNavigation.selectedItemId = R.id.navigation_rub
+            if (undoUrl.contains("=USD"))
+                bottomNavigation.selectedItemId = R.id.navigation_usd
+            if (undoUrl.contains("=EUR"))
+                bottomNavigation.selectedItemId = R.id.navigation_eur
         } else {
             super.onBackPressed()
         }
